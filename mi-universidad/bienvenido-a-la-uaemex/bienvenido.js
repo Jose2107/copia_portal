ae55e957418124e2450e609226f2018a7d80665e
JEmmanuelG0 <jgonzalezg086@alumno.uaemex.mx>
2025-02-05 18:35:02 -0600

    consejo universitario

--- mi-universidad/bienvenido-a-la-uaemex/bienvenido.js
+++ mi-universidad/bienvenido-a-la-uaemex/bienvenido.js
@@ -3,4 +3,26 @@ function showTab(index) {
     contents.forEach((content, i) => {
         content.classList.toggle('active', i === index);
     });
+    
+    let container = document.querySelector('.content-container');
+    let activeContent = document.querySelector('.tab-content.active');
+    container.style.height = '50px' + activeContent.scrollHeight + 'px';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showTab(0);
+});
+
+function showTab(tabIndex) {
+    // Primero, quitar la clase 'active' y 'active-tab' de todos los tabs
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
+    tabContents.forEach(content => content.classList.remove('active'));
+    
+    // Añadir la clase 'active' al contenido de la pestaña seleccionada
+    document.getElementById(`content-${tabIndex}`).classList.add('active');
+    
+    // Añadir la clase 'active-tab' al tab seleccionado
+    tabs[tabIndex].classList.add('active-tab');
 }
